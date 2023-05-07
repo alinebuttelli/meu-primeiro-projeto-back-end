4a9ef1fca969b7eea546a9b88424c1534b3ffde3
alinebuttelli <alinebuttelli@gmail.com>
2023-05-06 21:20:25 -0300

    adiciona a primeira versão do meu projeto

---
+++ mulheres.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router()
+
+const app = express()
+const porta = 3333
+
+const mulheres = [
+    {
+        nome: "Aline Buttelli",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.dreamstime.com%2Fprogramador-da-mulher-no-processo-do-trabalho-na-ilustra%25C3%25A7%25C3%25A3o-vetor-menina-nos-vidros-com-conceito-liso-estilo-port%25C3%25A1til-image139687164&psig=AOvVaw0sbC-inGEIESwhn4e-L_2t&ust=1683053554760000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNihoaPl1P4CFQAAAAAdAAAAABAE",
+        minibio: "Desenvolvedora em construção!" 
+    },
+    {
+        nome: "Iana Chan",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.dreamstime.com%2Fprogramador-da-mulher-no-processo-do-trabalho-na-ilustra%25C3%25A7%25C3%25A3o-vetor-menina-nos-vidros-com-conceito-liso-estilo-port%25C3%25A1til-image139687164&psig=AOvVaw0sbC-inGEIESwhn4e-L_2t&ust=1683053554760000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNihoaPl1P4CFQAAAAAdAAAAABAE",
+        minibio: "Fundadora da PrograMaria" 
+    },
+    {
+        nome: "Nina da Hora",
+        imagem: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.dreamstime.com%2Fprogramador-da-mulher-no-processo-do-trabalho-na-ilustra%25C3%25A7%25C3%25A3o-vetor-menina-nos-vidros-com-conceito-liso-estilo-port%25C3%25A1til-image139687164&psig=AOvVaw0sbC-inGEIESwhn4e-L_2t&ust=1683053554760000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNihoaPl1P4CFQAAAAAdAAAAABAE",
+        minibio: "Hacker antirracista" 
+    }
+]
+
+function mostraMulheres(request, response) {
+    response.json(mulheres)
+}
+
+function mostraPorta () {
+    console.log ("Servidor criado e rodando na porta ", porta)
+}
+
+app.use(router.get ("/mulheres", mostraMulheres))
+app.listen(porta, mostraPorta)
